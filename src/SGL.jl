b05a00941c89632a0b22c45756284ea130a81759
vataspet <vataspet@fel.cvut.cz>
2025-01-19 18:22:05 +0100

    initial commit

--- src/SGL.jl
+++ src/SGL.jl
@@ -2,4 +2,11 @@ module SGL
 
 # Write your package code here.
 
+# Export:
+# new_scene(width, height), add_triangle, add_sphere, add_point_light, camera, ray_trace, (anti_aliasing, lighting_model
+# secondary rays), reflection/refraction recursion depth (base 0)
+
+include("scene.jl")
+
+
 end
